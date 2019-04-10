2c4285334d5e94aadf3bcda3116cbf82788f216e
3032688016@qq.com <3032688016@qq.com>
2019-04-10 22:14:02 +0800

    testZyd

--- src/test/Demo1.java
+++ src/test/Demo1.java
@@ -6,6 +6,6 @@ package test;
 public class Demo1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello  World Git");
+        System.out.println("Hello  World Git2");
     }
 }
